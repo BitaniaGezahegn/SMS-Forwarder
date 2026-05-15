b35e699a9f945d141fba0c8b7c2d11ac7096e0d1
BT <bitaniagezahegn3@gmail.com>
2026-05-15 18:38:28 +0300

    Popup small change

--- config.js
+++ config.js
@@ -3,28 +3,27 @@
 
 window.SMSForwarderConfig = {
   // Selectors for identifying the active conversation
-  // The header element containing the contact's name/number.
-  chatHeaderSelector: 'mws-conversation-header .name, [data-e2e-conversation-name], .conversation-title, .mws-top-bar .title, mws-top-bar .title, h3', 
+  // Added generic h1, h2, span fallbacks and aria-labels
+  chatHeaderSelector: 'mws-conversation-header .name, [data-e2e-conversation-name], .conversation-title, .mws-top-bar .title, mws-top-bar .title, h3, h2, h1, [aria-label="Conversation Title"], mws-conversation-header span', 
   
   // The main container that holds all messages in the active chat
-  messageListSelector: 'mws-messages-list, .msg-list',
+  messageListSelector: 'mws-messages-list, .msg-list, [data-e2e-message-list], .message-list-container',
   
   // Selector for an individual message wrapper/container
-  messageWrapperSelector: 'mws-message-wrapper',
+  // Adding [data-e2e-message] and generic bubble classes
+  messageWrapperSelector: 'mws-message-wrapper, .message-bubble, [data-e2e-message], .msg-container',
   
   // Selector to identify incoming messages (vs outgoing)
-  // Usually, Google Messages has a class like 'incoming' or 'received' or a specific attribute.
-  // Alternatively, we can check for the absence of 'outgoing' / 'sent'.
-  incomingMessageSelector: 'mws-message-wrapper:not(.outgoing):not([is-sender="true"])',
+  // We check for absence of typical outgoing flags, or presence of incoming flags
+  incomingMessageSelector: 'mws-message-wrapper:not(.outgoing):not([is-sender="true"]):not([class*="sent"]), .message-bubble:not(.outgoing):not([is-sender="true"]), [data-e2e-is-sender="false"]',
 
   // Selector for the actual text content of the message
-  messageTextSelector: '.text-msg-content, .message-text, mws-message-part [dir="auto"]',
+  // Adding generic [dir="auto"] which Google uses heavily for text
+  messageTextSelector: '.text-msg-content, .message-text, mws-message-part [dir="auto"], [data-e2e-message-text], div[dir="auto"]',
   
   // Selector for the timestamp or metadata element (useful for deduplication hashing)
   messageTimestampSelector: '.message-timestamp, [data-e2e-message-timestamp], .timestamp, time, mws-tooltip, .status-text, [aria-label*=" AM"], [aria-label*=" PM"]',
   
   // Selector to identify if a message is SMS/MMS vs RCS
-  // This can be tricky. Often, the metadata or an icon indicates "SMS" or "RCS".
-  // Alternatively, we can look at the input box placeholder "Text message" vs "Chat message".
-  messageTypeIndicatorSelector: '.message-status, .metadata-text, mws-tooltip'
+  messageTypeIndicatorSelector: '.message-status, .metadata-text, mws-tooltip, [aria-label*="RCS"], [aria-label*="SMS"]'
 };
